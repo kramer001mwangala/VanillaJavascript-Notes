d5a67f3835363dcb55acec825eaab284eedc0faa
Kramer <Mwangala Kramer>
2023-10-19 19:22:24 -0400

    added while loops

---
+++ 9_while_loops/while_loops.js
@@ -0,0 +1,8 @@
+//in while loop we only specify the condition
+//this is a basic while loop structure
+'use strict'
+let kramer=1;
+while(kramer<=21){
+    console.log(`hi kramer this is mistake number ${kramer}`)
+    kramer++;
+}
