b011cf4c10ccbac892a4874cfa553df957abf979
Kramer <Mwangala Kramer>
2023-10-19 19:29:15 -0400

    while loop added

--- 9_while_loops/while_loops.js
+++ 9_while_loops/while_loops.js
@@ -6,3 +6,11 @@ while(kramer<=21){
     console.log(`hi kramer this is mistake number ${kramer}`)
     kramer++;
 }
+// repeats until the condition 6 is met with the dice
+//while loop is primarily used when your not sure how many times the loop will run
+let dice=Math.trunc(Math.random()*6)+1
+console.log(dice);
+while(dice !==6){
+console.log(`hey you rolled ${dice} would you mind trying again`);
+    dice=Math.trunc(Math.random()*6)+1;  
+}
