8a4d03be86bd093f83ff2b946679bec5ff441f0c
Kramer <Mwangala Kramer>
2023-10-19 18:49:08 -0400

    for loops beginning

---
+++ 8_Loops/for_loop.js
@@ -0,0 +1,11 @@
+/*a loop is a particular set of instrucions continiously repeated until a certain 
+instruction is met in this case its a for Loop 
+it normally has 3 parts (i)initial value
+                        (ii)counter
+                        (iii)Logic
+
+For loop keeps running until the condition is true*/
+// a sample for loop
+for (let kramer=1/*initial value*/; kramer<=20/*logic or condition*/; kramer++/*counter*/){
+    console.log(`kramer ndo kusema hii mtaa${kramer}au we unaonaje`)
+    }
