61dce731393eeba6e566f2bd0750225ce52177b4
Kramer <Mwangala Kramer>
2023-10-21 01:20:59 -0400

    ternary operators added

--- Basicagecalculator.js
+++ Basicagecalculator.js
@@ -1,5 +1,7 @@
-const jina=prompt("Enter Your Name");
-const yearOfBirth=prompt ("Enter Year of birth");
+const jina = prompt("Enter Your Name");
+const yearOfBirth = prompt("Enter Year of birth");
 const year = new Date().getFullYear();
-const age=year-yearOfBirth;
-const popup=alert("Hey "+jina+" congratulations you are "+age+" Years Old")
+const age = year - yearOfBirth;
+const popup = alert(
+  "Hey " + jina + " congratulations you are " + age + " Years Old"
+);
