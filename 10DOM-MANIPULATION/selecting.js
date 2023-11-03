cb321e271808abb9d1a480add5b46ef435fcc706
Kramer <Mwangala Kramer>
2023-11-03 10:29:22 -0400

    DOM manipulation intro

---
+++ 10DOM-MANIPULATION/selecting.js
@@ -0,0 +1,4 @@
+"use strict";
+//To select the preffered line we just use document.query selector and then we select the class or id
+document.querySelector(".kramer").textContent =
+  "kramer is good at manipulating DOM";
